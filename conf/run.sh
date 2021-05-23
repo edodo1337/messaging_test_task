1c441c6022a9d29ee6758cbe6b56d55f48fad640
Eldar100 <eldar100@mail.ru>
2021-05-23 15:37:01 +0300

    set superuser in run.sh

--- conf/run.sh
+++ conf/run.sh
@@ -2,4 +2,5 @@
 sleep 1
 python3 ./manage.py makemigrations
 python3 ./manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python manage.py shell
 gunicorn --forwarded-allow-ips=* --bind 0.0.0.0:8000 -w 2 app.wsgi:application
